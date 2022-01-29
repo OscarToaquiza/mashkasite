1c7323252c3133ea065a2f9b5b7974e8502d2bf8
Oscar Toaquiza <oscar.toaquiza.mashkasoft@gmail.com>
2022-01-28 23:36:05 -0500

    Cambios generales

---
+++ src/app/quienes-somos/personal.model.ts
@@ -0,0 +1,12 @@
+export class Persona {
+
+    nombre: string = "";
+    apellido: string = "";
+    cargo?: string;
+    email?: string;
+    url_foto?: string;
+    whatsapp?: string;
+    linkedin?: string;
+
+    constructor(){}
+}
--- src/app/quienes-somos/quienes-somos.component.ts
+++ src/app/quienes-somos/quienes-somos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Persona } from './personal.model';
 
 @Component({
   selector: 'app-quienes-somos',
@@ -7,9 +8,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuienesSomosComponent implements OnInit {
 
+
+  public equipo: Persona[] = [
+    {
+      nombre: "Richard",
+      apellido: "Gallardo",
+      cargo: "Desarrollador de Software",
+      email: "mailto:richard.gallardo.mashkasoft@gmail.com",
+      url_foto: "assets/img/equipo_1.jpg",
+      whatsapp: "https://wa.me/+593983898326",
+      linkedin: ""
+    },
+    {
+      nombre: "Oscar",
+      apellido: "Toaquiza",
+      cargo: "Desarrollador de Software",
+      email: "mailto:oscar.toaquiza.mashkasoft@gmail.com",
+      url_foto: "assets/img/equipo_1.jpg",
+      whatsapp: "https://wa.me/+593983898326",
+      linkedin: ""
+    },
+    {
+      nombre: "Mauro",
+      apellido: "Morales",
+      cargo: "Desarrollador Software",
+      email: "mailto:mauro.morales.mashkasoft@gmail.com",
+      url_foto: "assets/img/equipo_1.jpg",
+      whatsapp: "https://wa.me/+593983898326",
+      linkedin: ""
+    },
+    {
+      nombre: "Jenny",
+      apellido: "Rocha",
+      cargo: "Desarrollador Software",
+      email: "mailto:jenny.rocha.mashkasoft@gmail.com",
+      url_foto: "assets/img/equipo_1.jpg",
+      whatsapp: "https://wa.me/+593983898326",
+      linkedin: ""
+    },
+    {
+      nombre: "Ricardo",
+      apellido: "Muñoz",
+      cargo: "Desarrollador Software",
+      email: "mailto:ricardo.munioz.mashkasoft@gmail.com",
+      url_foto: "assets/img/equipo_1.jpg",
+      whatsapp: "https://wa.me/+593983898326",
+      linkedin: ""
+    },
+    {
+      nombre: "Daniel",
+      apellido: "Quingaluisa",
+      cargo: "Desarrollador Software",
+      email: "mailto:daniel.quingaluisa.mashkasoft@gmail.com",
+      url_foto: "assets/img/equipo_1.jpg",
+      whatsapp: "https://wa.me/+593983898326",
+      linkedin: ""
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
